a33ab15a6efc96934fddee84ec536ce4a073937a
janiceh94 <haesung94@gmail.com>
2023-01-10 12:29:47 -0800

    created two handle functions for user name and age

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,23 @@
 import Card from "../UI/Card/Card";
+import {useState} from "react";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button/Button";
 
 export default function AddUser(props){
 
+    const [enteredUsername, setEnteredUsername] = useState("");
+    const [enteredAge, setEnteredAge] = useState("");
+
+    const handleUsername = (e) => {
+        setEnteredUsername(e.target.value);
+    }
+    const handleAge = (e) => {
+        setEnteredAge(e.target.value);
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log(enteredUsername, enteredAge)
     }
 
     return (
@@ -13,9 +25,17 @@ export default function AddUser(props){
             <Card className={classes.input}>
                 <form onSubmit={handleSubmit}>
                     <label htmlFor="username">Username</label>
-                    <input id="username" type="text"/>
+                    <input 
+                        id="username" 
+                        type="text" 
+                        onChange={handleUsername}
+                    />
                     <label htmlFor="age">Age (Years)</label>
-                    <input id="age" type="number"/>
+                    <input 
+                        id="age" 
+                        type="number" 
+                        onChange={handleAge}
+                    />
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
